946e3a05188490104ca0dc4cbf14f679abbeda90
xiangyu <xiangyul@hotmail.com>
2022-03-27 22:18:46 +0800

    Add & Test: handle syntax error during parsing

--- parser.go
+++ parser.go
@@ -1,6 +1,8 @@
 package json2ast
 
-import "errors"
+import (
+    "errors"
+)
 
 type AstType uint8
 
@@ -10,27 +12,57 @@ const (
     Literal
 )
 
-type jsonLiteralAst jsonToken
+type nonTerminal uint8
+
+const (
+    parser nonTerminal = iota
+    element
+    array
+    elements
+    object
+    members
+)
+
+var firstSet = map[nonTerminal]map[tokenType]bool {
+    element: {LeftBrace: true, LeftBracket: true, String: true, Number: true, Boolean: true, Null: true},
+    array: {LeftBrace: true, LeftBracket: true, RightBracket: true ,String: true, Number: true, Boolean: true, Null: true},
+    elements: {RightBracket: true, Comma: true},
+    object: {RightBrace: true, String: true},
+    members: {RightBrace: true, Comma: true},
+}
+
+type literalAst jsonToken
 
 type JsonAst struct {
     ObjectAst   map[string]JsonAst
     ArrayAst    []JsonAst
-    LiteralAst  jsonLiteralAst
+    LiteralAst  literalAst
     Typ         AstType
 }
 
-var cursor = 0
+var cursor int
 var jts []jsonToken
 var jerrs []jsonError
 
-func Parser(source string) JsonAst {
+func Parser(source string) (JsonAst, []jsonError) {
     jts, jerrs = tokenize(source)
 
     if len(jerrs) != 0 || len(jts) == 0 {
-        return JsonAst{} // fuck
+        return JsonAst{}, jerrs
     }
+
     cursor = 0
-    return parseElement()
+    ast := parseElement(parser)
+    // expected end of json
+    if cursor < len(jts) {
+        jerrs = append(jerrs, jsonError{EndOfJsonExpected, jts[cursor].Loc})
+    }
+
+    if len(jerrs) != 0 {
+        ast = JsonAst{}
+    }
+
+    return ast, jerrs
 }
 
 func getToken() (jsonToken, error) {
@@ -44,29 +76,92 @@ func getToken() (jsonToken, error) {
     return token, errors.New("overstep")
 }
 
-func parseElement() JsonAst {
+func goPanic(nT nonTerminal, syncTokenTypes ...tokenType) (bool, bool, jsonToken) {
+    var sync = false
+    var first = false
+    var token jsonToken
+    var err error
+    for token, err = getToken(); err == nil; token, err = getToken() {
+        if firstSet[nT][token.Typ] {
+            first = true
+            break
+        }
+        for _, stt := range syncTokenTypes {
+            if token.Typ == stt {
+                sync = true
+                break // break the closest for
+            }
+        }
+        if sync { break }
+    }
+
+    return sync, first, token
+}
+
+func parseElement(caller nonTerminal) JsonAst {
     token, err := getToken()
     if err != nil {
-        // ValueExpected
-        // return JsonAst{}, errors.New("ValueExpected")
+        jerrs = append(jerrs, jsonError{ValueExpected, location{-1, -1}})
         return JsonAst{}
     }
 
+    if firstSet[element][token.Typ] {
+        return doParseElement(token)
+    }
+
+    if caller == parser {
+        jerrs = append(jerrs, jsonError{ValueExpected, token.Loc})
+        _, first, token := goPanic(element)
+        if first { _ = doParseElement(token) }
+        return JsonAst{}
+    }
+
+    if caller == elements {
+        if token.Typ == RightBrace || token.Typ == Colon {
+            jerrs = append(jerrs, jsonError{ValueExpected, token.Loc})
+            sync, first, token := goPanic(element, RightBracket, Comma)
+            if sync { cursor-- } // cursor go back to the Comma or RightBracket
+            if first { _ = doParseElement(token) }
+            return JsonAst{}
+        }
+
+        cursor-- // cursor go back to the Comma or RightBracket
+        if token.Typ == Comma { // Comma
+            jerrs = append(jerrs, jsonError{ValueExpected, token.Loc})
+        } else { // RightBracket
+            jerrs = append(jerrs, jsonError{TrailingComma, jts[cursor-1].Loc}) // Trailing comma
+        }
+        return JsonAst{}
+    }
+
+    if caller == object || caller == members {
+        jerrs = append(jerrs, jsonError{ValueExpected, token.Loc})
+        if token.Typ == RightBracket || token.Typ == Colon {
+            sync, first, token := goPanic(element, RightBrace, Comma)
+            if sync { cursor-- } // cursor go back to the Comma or RightBrace
+            if first { _ = doParseElement(token) }
+            return JsonAst{}
+        }
+
+        cursor-- // cursor go back to the Comma or RightBrace
+        return JsonAst{}
+    }
+
+    return JsonAst{}
+}
+
+func doParseElement(token jsonToken) JsonAst {
     var ast JsonAst
-    switch token.Typ {
-    case LeftBrace:
+
+    if token.Typ == LeftBrace {
         ast.ObjectAst = parseObject()
         ast.Typ = Object
-    case LeftBracket:
+    } else if token.Typ == LeftBracket {
         ast.ArrayAst = parseArray()
         ast.Typ = Array
-    case String: fallthrough
-    case Number: fallthrough
-    case Boolean: fallthrough
-    case Null:
-        ast.LiteralAst = jsonLiteralAst(token)
+    } else {
+        ast.LiteralAst = literalAst(token)
         ast.Typ = Literal
-    default: // TODO ERROR
     }
 
     return ast
@@ -75,133 +170,248 @@ func parseElement() JsonAst {
 func parseObject() map[string]JsonAst {
     token, err := getToken()
     if err != nil {
+        jerrs = append(jerrs, jsonError{PropertyOrClosingBraceExpected, location{-1, -1}})
         return nil
-        // return nil, errors.New("CommaOrClosingBraceExpected")
     }
-
-    var objectAst = make(map[string]JsonAst, 0)
 
     if token.Typ == RightBrace {
-        return objectAst
+        return map[string]JsonAst{}
     }
-
     if token.Typ == String {
-        var key = token.Val
-
-        token, err = getToken()
-        if err != nil {
-            return nil
-            // return nil, errors.New("ColonExpected")
-        }
-
-        if token.Typ == Colon {
-            objectAst[key] = parseElement()
-
-            others := parseObjMembers()
-            for k, v := range others { // TODO duplicated key
-                objectAst[k] = v
-            }
-
-            token, err = getToken()
-            if err != nil {
-                return nil
-                // return nil, errors.New("CommaOrClosingBraceExpected")
-            }
-
-            if token.Typ == RightBrace {
-                return objectAst
-            }
-        }
+        var objAst = doParseMember(token)
+        if isNextRightBrace() { return objAst }
+        return nil
     }
 
-    // TODO ERROR
+    jerrs = append(jerrs, jsonError{PropertyOrClosingBraceExpected, token.Loc})
+    cursor-- // for this token may also be sync tokens Comma or Colon
+    sync, first, token := goPanic(object, Comma, Colon)
+    if first {
+        if token.Typ == String {
+            _ = doParseMember(token)
+            _ = isNextRightBrace()
+        } // else RightBrace
+        return nil
+    }
+    if sync {
+        if token.Typ == Comma {
+            cursor-- // cursor go back to the Comma
+        } else { // Colon
+            parseElement(object)
+        }
+        _ = parseObjMembers()
+        _ = isNextRightBrace()
+        return nil
+    }
+
     return nil
 }
 
 func parseObjMembers() map[string]JsonAst {
     token, err := getToken()
     if err != nil {
+        jerrs = append(jerrs, jsonError{CommaOrClosingBraceExpected, location{-1, -1}})
         return nil
-        // return nil, errors.New("CommaOrClosingBraceExpected")
     }
 
+    if firstSet[members][token.Typ] {
+        return doParseObjMembers(token)
+    }
+
+    if token.Typ == String {
+        jerrs = append(jerrs, jsonError{CommaExpected, token.Loc})
+        _ = doParseMember(token)
+        return nil
+    }
+
+    jerrs = append(jerrs, jsonError{CommaOrClosingBraceExpected, token.Loc})
+    sync, first, token := goPanic(members, String)
+    if first {
+        _ = doParseObjMembers(token)
+        return nil
+    }
+    if sync {
+        _ = doParseMember(token)
+        return nil
+    }
+
+    return nil
+}
+
+func doParseObjMembers(token jsonToken) map[string]JsonAst {
     if token.Typ == RightBrace {
         cursor-- // ɛ
-        return nil
+        return map[string]JsonAst{}
     }
 
     if token.Typ == Comma {
-        token, err = getToken()
+        token, err := getToken()
         if err != nil {
+            jerrs = append(jerrs, jsonError{PropertyExpected, location{-1, -1}})
             return nil
-            // return nil, errors.New("PropertyExpected")
         }
 
-        var objectAst = make(map[string]JsonAst, 0)
         if token.Typ == String {
-            var key = token.Val
+            return doParseMember(token)
+        }
 
-            token, err = getToken()
-            if err != nil {
+        if token.Typ == Colon {
+            cursor-- // pretend to insert a String
+            jerrs = append(jerrs, jsonError{PropertyExpected, token.Loc})
+            _ = doParseMember(jsonToken{Val: "dummy"})
+            return nil
+        }
+
+        if token.Typ == Comma {
+            jerrs = append(jerrs, jsonError{PropertyExpected, token.Loc})
+            cursor-- // cursor go back to the Comma
+            _ = parseObjMembers()
+            return nil
+        }
+
+        if token.Typ == RightBrace {
+            cursor-- // cursor go back to the RightBrace
+            jerrs = append(jerrs, jsonError{TrailingComma, jts[cursor-1].Loc}) // Trailing comma
+            return nil
+        }
+
+        // other cases
+        jerrs = append(jerrs, jsonError{PropertyExpected, token.Loc})
+        sync, first, token := goPanic(members, String, Colon)
+        if first {
+            cursor-- // cursor go back to the Comma or RightBrace
+            _ = parseObjMembers()
+            return nil
+        }
+        if sync {
+            if token.Typ == String {
+                _ = doParseMember(token)
                 return nil
-                // return nil, errors.New("ColonExpected")
-            }
-
-            if token.Typ == Colon {
-                objectAst[key] = parseElement()
-
-                others := parseObjMembers()
-                for k, v := range others { // TODO duplicated key
-                    objectAst[k] = v
-                }
-                return objectAst
+            } else { // Colon
+                cursor-- // pretend to insert a string key
+                _ = doParseMember(jsonToken{Val: "dummy"})
+                return nil
             }
         }
+        return nil
     }
 
-    // TODO ERROR
     return nil
+}
+
+func doParseMember(token jsonToken) map[string]JsonAst {
+    var objAst = make(map[string]JsonAst)
+    var key = token.Val
+
+    token, err := getToken()
+    if err != nil {
+        jerrs = append(jerrs, jsonError{ColonExpected, location{-1, -1}})
+        return nil
+    }
+
+    if token.Typ == Colon {
+        objAst[key] = parseElement(object)
+        others := parseObjMembers()
+        for k, v := range others {
+            objAst[k] = v
+        }
+        return objAst
+    }
+
+    jerrs = append(jerrs, jsonError{ColonExpected, token.Loc})
+
+    if token.Typ == Comma {
+        cursor-- // cursor go back to the Comma
+        _ = parseObjMembers()
+        return nil
+    }
+
+    if token.Typ != Colon {
+        cursor-- // pretend to insert a Colon
+        parseElement(object)
+        _ = parseObjMembers()
+        return nil
+    }
+
+    return nil
+}
+
+func isNextRightBrace() bool {
+    token, err := getToken()
+    if err != nil || token.Typ != RightBrace {
+        if err != nil {
+            jerrs = append(jerrs, jsonError{CommaOrClosingBraceExpected, location{-1, -1}})
+        } else {
+            jerrs = append(jerrs, jsonError{CommaOrClosingBraceExpected, token.Loc})
+        }
+        return false
+    }
+    return true
 }
 
 func parseArray() []JsonAst {
     token, err := getToken()
     if err != nil {
+        jerrs = append(jerrs, jsonError{CommaOrClosingBracketExpected, location{-1, -1}})
         return nil
-        // return nil, errors.New("CommaOrClosingBracketExpected")
     }
 
+    if firstSet[array][token.Typ] {
+        return doParseArray(token)
+    }
+
+    jerrs = append(jerrs, jsonError{ValueExpected, token.Loc})
+    cursor-- // for this token may also be sync token Comma
+    sync, first, token := goPanic(array, Comma)
+    if first {
+        _ = doParseArray(token)
+        return nil
+    }
+    if sync {
+        cursor-- // cursor go back to the Comma
+        _ = parseAryElements()
+        _ = isNextRightBracket()
+        return nil
+    }
+
+    return nil
+}
+
+func doParseArray(token jsonToken) []JsonAst {
     var arrayAst = make([]JsonAst, 0)
 
     if token.Typ == RightBracket {
         return arrayAst
     }
 
-    if  token.Typ == LeftBrace || token.Typ == LeftBracket || token.Typ == String ||
-        token.Typ == Number || token.Typ == Boolean || token.Typ == Null {
-        cursor--
-        arrayAst = append(arrayAst, parseElement())
-        arrayAst = append(arrayAst, parseAryElements()...)
+    cursor-- // cursor back to the "element"
+    arrayAst = append(arrayAst, parseElement(array))
+    arrayAst = append(arrayAst, parseAryElements()...)
 
-        token, err = getToken()
-        if err != nil {
-            return nil
-            // return nil, errors.New("CommaOrClosingBracketExpected")
-        }
-
-        if token.Typ == RightBracket {
-            return arrayAst
-        }
+    if isNextRightBracket() {
+        return arrayAst
     }
-
-    // TODO ERROR
     return nil
+}
+
+func isNextRightBracket() bool {
+    token, err := getToken()
+    if err != nil || token.Typ != RightBracket {
+        if err != nil {
+            jerrs = append(jerrs, jsonError{CommaOrClosingBracketExpected, location{-1, -1}})
+        } else {
+            jerrs = append(jerrs, jsonError{CommaOrClosingBracketExpected, token.Loc})
+        }
+        return false
+    }
+    return true
 }
 
 func parseAryElements() []JsonAst {
     token, err := getToken()
     if err != nil {
+        jerrs = append(jerrs, jsonError{CommaOrClosingBracketExpected, location{-1, -1}})
         return nil
-        // return nil, errors.New("CommaOrClosingBracketExpected")
     }
 
     if token.Typ == RightBracket {
@@ -211,11 +421,33 @@ func parseAryElements() []JsonAst {
 
     if token.Typ == Comma {
         var arrayAst = make([]JsonAst, 0)
-        arrayAst = append(arrayAst, parseElement())
+        arrayAst = append(arrayAst, parseElement(elements))
         arrayAst = append(arrayAst, parseAryElements()...)
         return arrayAst
     }
 
-    // TODO ERROR
+    jerrs = append(jerrs, jsonError{CommaOrClosingBracketExpected, token.Loc})
+
+    if firstSet[element][token.Typ] {
+        cursor-- // cursor back to the "element"
+        _ = parseElement(elements)
+        _ = parseAryElements()
+        return nil
+    }
+
+    if token.Typ == RightBrace || token.Typ == Colon {
+        _, first, token := goPanic(elements)
+        if first {
+            if token.Typ == RightBracket {
+                cursor-- // cursor back to the RightBracket
+                return nil
+            } else { // Comma
+                _ = parseElement(elements)
+                _ = parseAryElements()
+                return nil
+            }
+        }
+    }
+
     return nil
 }
--- parser_test.go
+++ parser_test.go
@@ -1,54 +1,197 @@
 package json2ast
 
 import (
-    "fmt"
+    "encoding/json"
+    "reflect"
+    "strings"
     "testing"
 )
 
 func TestParser(t *testing.T) {
-    validTests := `
+    validTests := []string {
+    `
     {
         "bool1": true,
         "bool2": false,
         "Null": null,
         "Number0": 0,
+        "Number1": -0,
+        "Number2": 1,
+        "Number3": -1,
+        "Number4": 0.1,
+        "Number5": -0.1,
+        "Number6": 1234,
+        "Number7": -1234,
+        "Number8": 12.34,
+        "Number9": -12.34,
+        "Number10": 12E0,
+        "Number11": 12E1,
+        "Number12": 12e34,
+        "Number13": 12E-0,
+        "Number14": 12e+1,
+        "Number15": 12e-34,
+        "Number16": -12E0,
+        "Number17": -12E1,
+        "Number18": -12e34,
+        "Number19": -12E-0,
+        "Number20": -12e+1,
+        "Number21": -12e-34,
+        "Number22": 1.2E0,
+        "Number23": 1.2E1,
+        "Number24": 1.2e34,
+        "Number25": 1.2E-0,
+        "Number26": 1.2e+1,
+        "Number27": 1.2e-34,
+        "Number28": -1.2E0,
+        "Number29": -1.2E1,
+        "Number30": -1.2e34,
+        "Number31": -1.2E-0,
+        "Number32": -1.2e+1,
+        "Number33": -1.2e-34,
+        "Number34": 0E0,
+        "Number35": 0E1,
+        "Number36": 0e34,
+        "Number37": 0E-0,
+        "Number38": 0e+1,
+        "Number39": 0e-34,
+        "Number40": -0E0,
+        "Number41": -0E1,
+        "Number42": -0e34,
+        "Number43": -0E-0,
+        "Number44": -0e+1,
+        "Number45": -0e-34,
         "String0": "",
+        "String1": "abc",
+        "String2": "\\\b\f\r\n\t\"\u0030\"\t\n\r\f\b\\",
+        "String3": "\\\b\f\r\n\t\"\u0030\"\t\n\r\f\b\\abc",
+        "String4": "abc\\\b\f\r\n\t\"\u0030\"\t\n\r\f\b\\",
+        "String5": "abc\\\b\f\r\n\t\"\u0048\"\t\n\r\f\b\\abc",
+        "String6": "\\\\\\\"\"\"\"\\\"",
         "Array0": [
             {"type": "home", "number": "212 555-1234"},
-            {
-                "type": "fax",
-                "number": "646 555-4567"
-            }
+            {"type": "fax", "number": "646 555-4567"}
         ],
-        "object0":
-        {
+        "object0": {
             "streetAddress": "21 2nd Street",
             "city": "New York",
             "state": "NY",
             "postalCode": "10021"
         }
+    }`,
+    `123`,
+    `null`,
+    `true`,
+    `false`,
+    `"string"`,
+    `[123, null, true, false, "string", {"type": "home", "number": "212 555-1234"}]`,
     }
-    `
-    ast := Parser(validTests)
-    travel(ast)
+    for _, vt := range validTests {
+        if !json.Valid([]byte(vt)) {
+            t.Fatalf("valid test `%s` is invalid", vt)
+        }
+
+        ast, jerrs := Parser(vt)
+        if len(jerrs) != 0 {
+            t.Fatalf("build AST for `%s` failed", vt)
+        }
+
+        jsonText := ast2JsonText(ast)
+        if !json.Valid([]byte(jsonText)) {
+            t.Fatalf("json text `%s` is invalid, corresponding test is `%s`", jsonText, vt)
+        }
+
+        if ast.Typ == Object {
+            vtObj := make(map[string]interface{})
+            astObj := make(map[string]interface{})
+            doTestParser(t, vt, vtObj, jsonText, astObj)
+            continue
+        }
+
+        if ast.Typ == Array {
+            vtObj := make([]interface{}, 0)
+            astObj := make([]interface{}, 0)
+            doTestParser(t, vt, vtObj, jsonText, astObj)
+            continue
+        }
+
+        if ast.Typ == Literal {
+            if jsonText != vt {
+                t.Fatalf("json object of `%s` is not equal to `%s`", jsonText, vt)
+            }
+            continue
+        }
+
+        t.Fatalf("unsupported AST type: %d", ast.Typ)
+    }
+
+    invalidTests := []string {
+        `{{[], "k1":{[]{:123}, "k2":{][{, "k3":123}, "k4":{1 true null false: 123}, "k5":{, "k6":123}, "k7":{:123}}`,
+        `{"k1":123 "k2":123, "k3":123, "k4":{"k5":123 {][ 123 true false null:, "k6": 123 }}`,
+        `{"k1":[}:}:, 123], "k2":[:}:}, 123], "k3":[,123,123], "k4":[}:"v1"], "k5":[:}123], "k6":[:}true], "k7":[:}[]]}`,
+        `{"k1":[}}{"k2":123}], "k3":[}}{"k4":123}], "k5":[}}null, 123]}`,
+        `{"k1":[123}:}:], "k2":[123}:}:, 123], "k3":[123:}:}{"k4":123}, 123], "k5":[123}:}:[123,123]], "k6":[123 123]}`,
+        `{"k1":[123 "v1"], "k2":[123 true], "k3":[123 false], "k4":[123 null]}`,
+        `{"k1":[123,], "k2":[123,}:}::,123], "k3":[123,:}:{}], "k4":[123,:}:[123]], "k5":[123,:}:"v1"], "k6":[123,:}:123]}`,
+        `{"k1":[123,}:false], "k2":[123,}:true], "k3":[123,}:null]}, "k4":[123,,456]}`,
+        `{"k1":{"k2"::]:]}, "k3":}}`,
+        `{"k1":]:], "k2":123, "k3":{"k4":,}}`,
+        `,:}]{"k1":123}{},:[] "k1" 123 null false true`,
+    }
+
+    for _, ivt := range invalidTests {
+        if json.Valid([]byte(ivt)) {
+            t.Fatalf("invalid test `%s` is valid", ivt)
+        }
+
+        _, jerrs := Parser(ivt)
+        t.Logf("errors of `%s` :", ivt)
+        for _, jerr := range jerrs {
+            t.Log(jerr)
+        }
+        t.Log()
+    }
 }
 
-func travel(ast JsonAst) {
+func doTestParser(t *testing.T, vt string, vtObj interface{}, jsonText string, astObj interface{}) {
+    err := json.Unmarshal([]byte(vt), &vtObj)
+    if err != nil {
+        t.Fatalf("unmarshal valid test `%s` failed", vt)
+    }
+
+    err = json.Unmarshal([]byte(jsonText), &astObj)
+    if err != nil {
+        t.Fatalf("unmarshal json text `%s` failed, corresponding test is `%s`", jsonText, vt)
+    }
+
+    if !reflect.DeepEqual(astObj, vtObj) {
+        t.Fatalf("json object of `%s` is not equal to `%s`", jsonText, vt)
+    }
+}
+
+func ast2JsonText(ast JsonAst) string {
+    var sb strings.Builder
     switch ast.Typ {
     case Object:
-        fmt.Printf("{")
+        var i = 0
+        sb.WriteRune('{')
         for k, v := range ast.ObjectAst {
-            fmt.Printf("%s: ", k)
-            travel(v)
+            sb.WriteString(k)
+            sb.WriteString(": ")
+            sb.WriteString(ast2JsonText(v))
+            if i != len(ast.ObjectAst)-1 { sb.WriteString(", ") }
+            i++
         }
-        fmt.Printf("}, ")
+        sb.WriteRune('}')
     case Array:
-        fmt.Printf("[")
-        for _, v := range ast.ArrayAst {
-            travel(v)
+        sb.WriteRune('[')
+        for i, v := range ast.ArrayAst {
+            sb.WriteString(ast2JsonText(v))
+            if i != len(ast.ArrayAst)-1 { sb.WriteString(", ") }
         }
-        fmt.Printf("], ")
+        sb.WriteRune(']')
     case Literal:
-        fmt.Printf("%s, ", ast.LiteralAst.Val)
+        sb.WriteString(ast.LiteralAst.Val)
     }
+
+    return sb.String()
 }
